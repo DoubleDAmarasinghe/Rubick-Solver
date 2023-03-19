48574e28d6d34d054dffe443e153872069074e8f
Dilanga A.L.D <73058707+DoubleDAmarasinghe@users.noreply.github.com>
2023-03-20 04:12:02 +0530

    Solve fixed with whole cube rotation

--- Assets/Scripts/CubeRotationManager.cs
+++ Assets/Scripts/CubeRotationManager.cs
@@ -21,11 +21,17 @@ public class CubeRotationManager : MonoBehaviour
     {
         readCube = GameObject.FindObjectOfType<ReadCube>();
         automate = GameObject.FindObjectOfType<Automate>();
+        // cube.transform.Rotate(0, 90, 0, Space.World);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        
+
+        //Swipe();
+        //Drag();
         if(transform.rotation != cube.transform.rotation)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, cube.transform.rotation, speed * Time.deltaTime);
@@ -83,15 +89,54 @@ public class CubeRotationManager : MonoBehaviour
 
     public void RotateUp()
     {
-        cube.transform.Rotate(90, 0, 0, Space.World);
+        cube.transform.Rotate(0, 0, -90, Space.World);
         wholeCubeRotating = true;
         controllerArrowPanel.SetActive(false);
     }
 
     public void RotateBottom()
     {
-        cube.transform.Rotate(-90, 0, 0, Space.World);
+        cube.transform.Rotate(0, 0, 90, Space.World);
         wholeCubeRotating = true;
         controllerArrowPanel.SetActive(false);
     }
+
+    // void Drag()
+    // {
+    //     // if (Input.GetMouseButton(1))
+    //     // {
+    //     //     // while the mouse is held down the cube can be moved around its central axis to provide visual feedback
+    //     //     mouseDelta = Input.mousePosition - previousMousePosition;
+    //     //     mouseDelta *= 0.1f; // reduction of rotation speed
+    //     //     transform.rotation = Quaternion.Euler(mouseDelta.y, -mouseDelta.x, 0) * transform.rotation;
+    //     // }
+    //     // else
+    //     // {
+    //         // automatically move to the target position
+    //         if (transform.rotation != cube.transform.rotation)
+    //         {
+    //             var step = speed * Time.deltaTime;
+    //             transform.rotation = Quaternion.RotateTowards(transform.rotation, cube.transform.rotation, step);
+    //         }
+    // }
+
+    // public void WholeRotateLeft()
+    // {
+    //     cube.transform.Rotate(0, 90, 0, Space.World);
+    // }
+
+    // public void WholeRotateRight()
+    // {
+    //     cube.transform.Rotate(0, -90, 0, Space.World);
+    // }
+
+    // public void WholeRotateUp()
+    // {
+    //     cube.transform.Rotate(0, 0, -90, Space.World);
+    // }
+
+    // public void WholeRotateDown()
+    // {
+    //     cube.transform.Rotate(0, 0, 90, Space.World);
+    // }
 }
