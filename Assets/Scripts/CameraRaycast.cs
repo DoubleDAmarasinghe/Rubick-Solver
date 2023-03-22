7fd499f4372be3025cc21c4f9145405cea1c5486
Dilanga A.L.D <73058707+DoubleDAmarasinghe@users.noreply.github.com>
2023-03-22 06:22:07 +0530

    Face Detection 1

---
+++ Assets/Scripts/CameraRaycast.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycast : MonoBehaviour
+{
+    private  int layerMask_red = 1 << 7;
+    private  int layerMask_blue = 1 << 8;
+    public GameObject arCamera;
+    public GameObject controllerArrows;
+
+    public GameObject redSideMesh;
+    public GameObject blueSideMesh;
+    bool redSide = false;
+    bool blueSide = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 startPosition = arCamera.transform.position;
+        RaycastHit hit;
+        //startPosition.z = 100f;
+        //startPosition = arCamera.ScreenToWorldPoint(startPosition);
+        Ray rayFromFront = new Ray(arCamera.transform.position, arCamera.transform.forward);
+        if(Physics.Raycast(rayFromFront, out hit, 10000f, layerMask_red) && !redSide)
+        {
+            //controllerArrows.transform.position = redSideMesh.transform.position;
+            //controllerArrows.transform.rotation = redSideMesh.transform.rotation;
+            controllerArrows.transform.rotation = redSideMesh.transform.rotation;
+            //controllerArrows.transform.Rotate(0,90,0,Space.World);
+            redSide = true;
+            //blueSide = false;
+        }
+
+        if(Physics.Raycast(rayFromFront, out hit, 10000f, layerMask_blue) && !blueSide)
+        {
+            controllerArrows.transform.position = blueSideMesh.transform.position;
+            //controllerArrows.transform.rotation = redSideMesh.transform.rotation;
+            //controllerArrows.transform.Rotate(0,-90,0,Space.World);
+            Debug.Log("eeeeeeeeeeeeeeeeee");
+            blueSide = true;
+            //redSide = false;
+        }
+
+        Debug.DrawRay(arCamera.transform.position, arCamera.transform.forward,Color.green);
+    }
+}
