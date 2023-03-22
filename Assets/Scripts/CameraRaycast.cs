59c2ab18ad2982dca6cd4c8c8a4df25b6aa4ef2e
Dilanga A.L.D <73058707+DoubleDAmarasinghe@users.noreply.github.com>
2023-03-22 13:14:16 +0530

    Six side detection complete

--- Assets/Scripts/CameraRaycast.cs
+++ Assets/Scripts/CameraRaycast.cs
@@ -6,13 +6,25 @@ public class CameraRaycast : MonoBehaviour
 {
     private  int layerMask_red = 1 << 7;
     private  int layerMask_blue = 1 << 8;
+    private  int layerMask_green = 1 << 9;
+    private  int layerMask_yellow = 1 << 10;
+    private  int layerMask_white = 1 << 11;
+    private  int layerMask_orange = 1 << 12;
     public GameObject arCamera;
     public GameObject controllerArrows;
 
     public GameObject redSideMesh;
     public GameObject blueSideMesh;
+    public GameObject greenSideMesh;
+    public GameObject yellowSideMesh;
+    public GameObject whiteSideMesh;
+    public GameObject orangeSideMesh;
     bool redSide = false;
     bool blueSide = false;
+    bool greenSide = false;
+    bool yellowSide = false;
+    bool whiteSide = false;
+    bool orangeSide = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,26 +39,86 @@ public class CameraRaycast : MonoBehaviour
         //startPosition.z = 100f;
         //startPosition = arCamera.ScreenToWorldPoint(startPosition);
         Ray rayFromFront = new Ray(arCamera.transform.position, arCamera.transform.forward);
+        
+        
+        
+        
         if(Physics.Raycast(rayFromFront, out hit, 10000f, layerMask_red) && !redSide)
         {
-            //controllerArrows.transform.position = redSideMesh.transform.position;
-            //controllerArrows.transform.rotation = redSideMesh.transform.rotation;
+            
             controllerArrows.transform.rotation = redSideMesh.transform.rotation;
-            //controllerArrows.transform.Rotate(0,90,0,Space.World);
             Debug.Log("RedSide");
-            //controllerArrows.transform.Rotate(0,90,0,Space.World);
             redSide = true;
+            orangeSide = false;
+            whiteSide = false;
+            greenSide = false;
             blueSide = false;
+            yellowSide = false;
         }
 
         if(Physics.Raycast(rayFromFront, out hit, 10000f, layerMask_blue) && !blueSide)
         {
-            //controllerArrows.transform.Rotate(0,-90,0,Space.World);
+            
             controllerArrows.transform.rotation = blueSideMesh.transform.rotation;
-            //controllerArrows.transform.Rotate(0,-90,0,Space.World);
             Debug.Log("BlueSide");
             blueSide = true;
             redSide = false;
+            whiteSide = false;
+            greenSide = false;
+            orangeSide = false;
+            yellowSide = false;
+        }
+
+        if(Physics.Raycast(rayFromFront, out hit, 10000f, layerMask_green) && !greenSide)
+        {
+            
+            controllerArrows.transform.rotation = greenSideMesh.transform.rotation;
+            Debug.Log("GreenSide");
+            greenSide = true;
+            redSide = false;
+            whiteSide = false;
+            orangeSide = false;
+            blueSide = false;
+            yellowSide = false;
+        }
+
+        if(Physics.Raycast(rayFromFront, out hit, 10000f, layerMask_yellow) && !yellowSide)
+        {
+            
+            controllerArrows.transform.rotation = yellowSideMesh.transform.rotation;
+            Debug.Log("YellowSide");
+            yellowSide = true;
+            redSide = false;
+            whiteSide = false;
+            greenSide = false;
+            blueSide = false;
+            orangeSide = false;
+        }
+
+        if(Physics.Raycast(rayFromFront, out hit, 10000f, layerMask_white) && !whiteSide)
+        {
+            
+            controllerArrows.transform.rotation = whiteSideMesh.transform.rotation;
+            Debug.Log("WhiteSide");
+            whiteSide = true;
+            redSide = false;
+            greenSide = false;
+            blueSide = false;
+            yellowSide = false;
+            orangeSide = false;
+        }
+
+        if(Physics.Raycast(rayFromFront, out hit, 10000f, layerMask_orange) && !orangeSide)
+        {
+            
+            controllerArrows.transform.rotation = orangeSideMesh.transform.rotation;
+            Debug.Log("OrangeSide");
+            orangeSide = true;
+            redSide = false;
+            whiteSide = false;
+            greenSide = false;
+            blueSide = false;
+            yellowSide = false;
         }
 
         Debug.DrawRay(arCamera.transform.position, arCamera.transform.forward,Color.green);
